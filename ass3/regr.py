d27b844cc24b1a28d7e038f8d80b2dd886b2cfff
Shilpa <shilpasanjeev1@gmail.com>
2018-10-20 15:11:46 +1100

    func returns prediction given area, crime

--- ass3/regr.py
+++ ass3/regr.py
@@ -4,10 +4,32 @@ from sklearn.metrics import mean_squared_error
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 import numpy as np
+from math import *
+
+#read file (df is global)
+df = pd.read_csv('crimes.csv')
+
+#to select required columns (years: Jan 2007 - Dec 2012)
+def select_cols_for_ml():
+    cols = []
+    cols.append(0)
+    cols.append(1)
+    for i in range(146, 218):
+        cols.append(i)
+    return cols
+
+#for years : jan 2007 - dec 2011
+def select_cols_for_training():
+    cols = []
+    #cols.append(0)
+    for i in range(2, 62):
+        cols.append(i)
+    return cols
 
 
-def data_cleaning():
-	df = pd.read_csv('crimes.csv')
+def get_data():
+	
+	global df
 	#index starts from 1
 	df.index+=1
 	df = df.drop('Subcategory', axis=1)
@@ -15,20 +37,67 @@ def data_cleaning():
 	#drop last 4 useless rows
 	df = df[:-4]
 	#list of unique offences in the table 
-	count = 1
-	for i in df.Offence_category.unique():
-	    print(count, i)
-	    count+=1
+	#count = 1
+	#for i in df.Offence_category.unique():
+	#    print(count, i)
+	#    count+=1
 	#list of unique LGAs
-	count = 1
-	for lga in df.LGA.unique():
-	    print(count, lga)
-	    count+=1
+	#count = 1
+	#for lga in df.LGA.unique():
+	#    print(count, lga)
+	#    count+=1
 	#get sum of number of offences of each category in each lga
 	df = df.groupby(['LGA','Offence_category']).sum().reset_index()
-	df.index+=1
+	
+	#select rqd cols: index, area, offence and years: Jan 2007 - Dec 2012
+	df = df.iloc[:, select_cols_for_ml()]
+	
 
-if __name__ == "__main__":
-	#pre process data
-	data_cleaning()
 
+def prepare_model():
+
+	global df
+
+	df = shuffle(df)
+	df = df.reset_index()
+	df = df.drop('index', axis=1)
+	#all values from 2007 - 2011 for training
+	crime_x = df.iloc[:, select_cols_for_training()].values
+	#values from jan 2012 as target to be predicted
+	crime_y = df[['Jan 2012']].values
+
+	
+	return crime_x, crime_y
+
+	
+def get_crime_prediction(lga, crime_type):
+
+	global df
+	get_data()
+
+	df = df.loc[df['Offence_category'] == crime_type]
+	crime_x, crime_y = prepare_model()
+
+	model = linear_model.LinearRegression()
+	model.fit(crime_x, crime_y)
+	y_pred = model.predict(crime_x)
+
+	mylist = {}
+	j = 0
+	for i in range(len(crime_x)):
+	        area = df.iloc[j,0]	        
+	        predicted_crime_rate = float("{0:.2f}".format(fabs(y_pred[i])))
+	        #print(j, " ", area," ", "Expected:",crime_y[i], "Predicted: %.2f"% fabs(y_pred[i]))	        
+	        mylist[area] = predicted_crime_rate
+	        j+=1
+
+	#print("Mean squared error: %.2f" % mean_squared_error(crime_y, y_pred))
+
+
+	prediction = mylist[lga]
+
+	return prediction
+	
+
+prediction = get_crime_prediction("Woollahra", "Theft")
+print(prediction)
